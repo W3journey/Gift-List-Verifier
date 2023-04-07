61ba625c80c7cc93d4e038ac00117884a95e271f
W3journey <web3journey@proton.me>
2023-04-07 17:13:30 +0200

    initial commit

---
+++ client/index.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+
+const serverUrl = "http://localhost:1225";
+
+async function main() {
+  const merkleTree = new MerkleTree(niceList);
+  const name = "James Schuster";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    name: name,
+    proof: proof,
+  });
+
+  console.log({ gift });
+}
+
+main();
